7ccedeca08b69d27bcbe24eb54ba75cdea938471
eduardomsj <eduardomsj@gmail.com>
2019-05-16 01:11:00 -0300

    Projeto criado

---
+++ src/main/java/com/eduardojr/conectaris/ConectarisApplication.java
@@ -0,0 +1,13 @@
+package com.eduardojr.conectaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectarisApplication.class, args);
+	}
+
+}
