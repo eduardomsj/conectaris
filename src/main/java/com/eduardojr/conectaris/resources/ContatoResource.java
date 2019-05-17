eee424e2a339cc83d5e2a142309d83c9dacb5df8
eduardomsj <eduardomsj@gmail.com>
2019-05-16 23:18:40 -0300

    Testando primeira classe de dominio - Contato

--- src/main/java/com/eduardojr/conectaris/resources/ContatoResource.java
+++ src/main/java/com/eduardojr/conectaris/resources/ContatoResource.java
@@ -1,16 +1,29 @@
 package com.eduardojr.conectaris.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardojr.conectaris.domain.Contato;
 
 @RestController
 @RequestMapping(value="/contatos")
 public class ContatoResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST OK";
+	public List<Contato> listar() {
+
+		Contato c1 = new Contato(1, "Eduardo", "TI");
+		Contato c2 = new Contato(1, "Júnior", "RH");
+		
+		List<Contato> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 
 }
