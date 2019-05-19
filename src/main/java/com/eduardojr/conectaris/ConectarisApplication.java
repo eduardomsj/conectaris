b66d0d5b8dbf55d4bc0a1d3d061f78550003996d
eduardomsj <eduardomsj@gmail.com>
2019-05-19 02:32:24 -0300

    Criando operacao de instaciacao

--- src/main/java/com/eduardojr/conectaris/ConectarisApplication.java
+++ src/main/java/com/eduardojr/conectaris/ConectarisApplication.java
@@ -1,13 +1,32 @@
 package com.eduardojr.conectaris;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ConectarisApplication {
+import com.eduardojr.conectaris.domain.Contato;
+import com.eduardojr.conectaris.repositories.ContatoRepository;
 
+@SpringBootApplication
+public class ConectarisApplication implements CommandLineRunner {
+
+	@Autowired
+	private ContatoRepository contatoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConectarisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Contato c1 = new Contato(null, "Vinicius", null);
+		Contato c2 = new Contato(null, "Marcia", null);
+		
+		contatoRepository.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
